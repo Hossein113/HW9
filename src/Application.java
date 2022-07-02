590cb3397db75679c6362786460862157a23dcb1
Hossein113 <hos_m18@yahoo.com>
2022-07-02 14:26:27 +0430

    update Application

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Application {
@@ -9,7 +10,8 @@ public class Application {
     public static void main(String[] args) {
 
 
-        List<String> nameList = Arrays.asList("Amir", "Hatef", "Mehran", "Mojtaba", "Mohammad", "Ali", "Davood", "Reza", "Mohsen");
+        List<String> nameList = Arrays.asList("Amir", "Hatef", "Mehran", "Mojtaba",
+                "Mohammad", "Ali", "Davood", "Reza", "Mohsen");
 
         List<List<String>> newList = new ArrayList<>();
 
@@ -22,17 +24,27 @@ public class Application {
 
             if (!collect.isEmpty()) {
                 newList.add(collect);
+
             }
         }
         newList.forEach(System.out::println);
+        System.out.println("");
+
+
+        System.out.println("*****************************************");
+        System.out.println("");
+
+        Map<Integer, List<String>> collect = nameList.stream().collect(Collectors.groupingBy(word -> word.length()));
+
+        Map<Integer, List<List<String>>> collect1 = collect.values().stream().collect(Collectors.groupingBy(word -> word.size()));
+
+        System.out.println(collect);
+        System.out.println("");
+        System.out.println(collect1);
+
+
     }
 
-
-
-
-
-
-
-
 }
+
 
